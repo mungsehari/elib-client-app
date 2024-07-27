988af11e97d427bfd28477101c714aa78e4c7a65
mungsehari <mungse.hari@gmail.com>
2024-07-27 14:21:43 +0530

    add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
+import FooterPage from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar/>
-        {children}</body>
+        <Navbar />
+        {children}
+        <FooterPage />
+      </body>
     </html>
   );
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FooterPage = () => {
+  return (
+    <footer className="border-t p-5 text-center">
+      <span>@ 2024 Code&#39;s Book All right reseved . </span>
+    </footer>
+  );
+};
+
+export default FooterPage;
