ee7118ccccdc039f3ff1e6f1c6023611b14f1ec0
mungsehari <mungse.hari@gmail.com>
2025-01-06 12:02:50 +0530

    add booklist

--- src/app/(home)/components/book-list.tsx
+++ src/app/(home)/components/book-list.tsx
@@ -1,21 +1,30 @@
+"use client";
 import { Book } from "@/types";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import BookCard from "./book-card";
 
-const BookList = async () => {
-  const response = await fetch(`http://localhost:5050/books`, {
-    cache: "no-store",
-  });
-  if (!response.ok) {
-    throw new Error("An error occurred while fetching books");
-  }
-  const books = await response.json();
-  console.log("books: ", books);
+const BookList = () => {
+  const [books, setBooks] = useState([]);
+  const fetchBooks = async () => {
+    const response = await fetch(`http://localhost:5501/api/books`, {
+      cache: "no-store",
+    });
+    if (!response.ok) {
+      throw new Error("An error occurred while fetching books");
+    }
+    const books = await response.json();
+    console.log("books: ", books);
+    setBooks(books);
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
 
   return (
     <div className="grid grid-cols-1 gap-8 md:grid-cols-3 max-w-7xl mx-auto mb-10">
-      {books.map((book: Book) => (
-        <BookCard key={book.id} book={book} />
+      {(books || []).map((book: Book) => (
+        <BookCard key={book._id} book={book} />
       ))}
     </div>
   );
